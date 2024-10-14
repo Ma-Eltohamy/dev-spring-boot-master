a08375bd953d80695b042de4cabdfeb3becdab71
ma-tohamey <mahmoud.altohamey@aol.com>
2024-10-14 05:32:46 +0300

    running spring using command line

---
+++ 01-spring-boot-overview/05-command-line-demo/src/main/java/com/mahmoud/springboot/demo/RestControllers/FunRestController.java
@@ -0,0 +1,28 @@
+package com.mahmoud.springboot.demo.RestControllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello, spring-boot!";
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 1k!";
+    }
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day.";
+    }
+
+
+
+}
