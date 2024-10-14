81a8c866d8eb0d50864c8c0c8010fa93e5e1c64f
ma-tohamey <mahmoud.altohamey@aol.com>
2024-10-14 05:01:47 +0300

    actuator-demo

---
+++ 01-spring-boot-overview/03-actuator-demo/src/test/java/com/mahmoud/springboot/demo/MyCoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmoud.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
